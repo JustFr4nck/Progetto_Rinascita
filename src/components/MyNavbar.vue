3d941613edc7d77c77687f2f84d87aa0d84eba34
JustFr4nck <francyperry07@gmail.com>
2025-11-22 17:46:02 +0100

    main page release

---
+++ src/components/MyNavbar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="flex flex-row justify-center  pt-3">
+        <a class=" textBar font-bold " href="">Chi siamo&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
+        <a class=" textBar font-bold " href="">Progetti</a>
+    </div>
+</template>
+
+<style scoped>
+
+.textBar{
+
+  color: #042C54;
+}
+
+.textBar:hover{
+
+  color: #FFC787;
+}
+
+.textBar:active{
+
+  color: #042C54;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,51 @@
 <template>
-  <div class=" flex flex-col  items-center backGroundMain">
-    <div class="pt-10">
-      <img class="rounded-full imgMain" :src="iconMeuList" alt="">
+  <div>
+    <MyNavbar/>
+  </div>
+
+  <div class="flex flex-col md:flex-row items-center backGroundMain p-6 md:p-10 gap-6">
+
+
+    <div class="flex shrink-0">
+      <img
+        class="rounded-full object-cover imgMain"
+        :src="iconMeuList"
+        alt="Icona"
+      />
     </div>
+
+
+    <div class="flex flex-col text-center md:text-left lg:ml-10">
+      <h1 class="text-3xl font-bold textTit roboto-slab">PROGETTO MEUCCI</h1>
+      <h2 class="text-2xl font-bold textSub roboto-slab">"La scuola che vogliamo, la costruiamo insieme"</h2>
+      <p class="text-xl  mt-2 textTit roboto-slab">
+        La nostra lista si impegna a migliorare la scuola attraverso progetti concreti e inclusivi,
+        <br>pensati per valorizzare ogni studente.
+        <br>Vogliamo rendere l’ambiente scolastico più accogliente,
+        <br>sicuro e stimolante, promuovendo attività che favoriscano collaborazione, partecipazione e benessere.
+        <br>Il nostro obiettivo è costruire una comunità scolastica più moderna,
+        <br>attenta alle esigenze di tutti e capace di offrire nuove opportunità di crescita.
+      </p>
+    </div>
+
   </div>
 </template>
 
 <script setup>
-  import iconMeuList from '@/assets/iconMeuList.png';
+import iconMeuList from '@/assets/iconMeuList.png';
+import MyNavbar from '@/components/MyNavbar.vue';
 </script>
+
+
+<style scoped>
+
+.textTit{
+
+  color: #042C54;
+}
+
+.textSub{
+  color: #FFC787
+}
+
+</style>
